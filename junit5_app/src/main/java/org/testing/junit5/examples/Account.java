a6c1a96753f079aa6cd5991150c4bf2a7d7b3024
Julian da Costa Faro <juliandacostafaro@gmail.com>
2023-03-10 14:52:04 -0300

    TDD debit and credit on account

--- junit5_app/src/main/java/org/testing/junit5/examples/Account.java
+++ junit5_app/src/main/java/org/testing/junit5/examples/Account.java
@@ -1,6 +1,7 @@
 package org.testing.junit5.examples;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Account {
     private String person;
@@ -25,5 +26,21 @@ public class Account {
 
     public void setBalance(BigDecimal balance) {
         this.balance = balance;
+    }
+
+    public void debit(BigDecimal amount) {
+        balance = balance.subtract(amount);
+    }
+
+    public void credit(BigDecimal amount) {
+        balance = balance.add(amount);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return person.equals(account.person) && balance.equals(account.balance);
     }
 }
--- junit5_app/src/test/java/org/testing/junit5/examples/AccountTest.java
+++ junit5_app/src/test/java/org/testing/junit5/examples/AccountTest.java
@@ -21,4 +21,29 @@ class AccountTest {
         assertEquals(1000.12345, account.getBalance().doubleValue());
         assertFalse(account.getBalance().compareTo(BigDecimal.ZERO) < 0);
     }
+
+    @Test
+    void testAccountReference() {
+        Account account = new Account("Jonh Doe", new BigDecimal("8900.9997"));
+        Account account2 = new Account("Jonh Doe", new BigDecimal("8900.9997"));
+        assertEquals(account, account2);
+    }
+
+    @Test
+    void test_accountDebit() {
+        Account account = new Account("Julian", new BigDecimal("1000.12345"));
+        account.debit(new BigDecimal(100));
+        assertNotNull(account.getBalance());
+        assertEquals(900, account.getBalance().intValue());
+        assertEquals("900.12345", account.getBalance().toPlainString());
+    }
+
+    @Test
+    void test_accountCredit() {
+        Account account = new Account("Julian", new BigDecimal("1000.12345"));
+        account.credit(new BigDecimal(100));
+        assertNotNull(account.getBalance());
+        assertEquals(1100, account.getBalance().intValue());
+        assertEquals("1100.12345", account.getBalance().toPlainString());
+    }
 }
