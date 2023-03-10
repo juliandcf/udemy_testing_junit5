6bfc935513c59a81f8fbb6416c61add997da56a2
Julian da Costa Faro <juliandacostafaro@gmail.com>
2023-03-10 14:35:12 -0300

    add simple test balance account

--- junit5_app/src/test/java/org/testing/junit5/examples/AccountTest.java
+++ junit5_app/src/test/java/org/testing/junit5/examples/AccountTest.java
@@ -3,13 +3,22 @@ package org.testing.junit5.examples;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class AccountTest {
 
     @Test
     void test_nameAccount() {
-        Account account = new Account("Julian", BigDecimal.valueOf(123.12));
+        Account account = new Account("Julian", new BigDecimal("1000.12345"));
         String expected = "Julian";
         assertEquals(expected, account.getPerson());
+    }
+
+    @Test
+    void test_balanceAccount() {
+        Account account = new Account("Julian", new BigDecimal("1000.12345"));
+        assertEquals(1000.12345, account.getBalance().doubleValue());
+        assertFalse(account.getBalance().compareTo(BigDecimal.ZERO) < 0);
     }
 }
