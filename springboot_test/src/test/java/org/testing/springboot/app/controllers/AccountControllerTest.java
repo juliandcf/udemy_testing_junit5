a7ea91dff271f74fcb7cff4da8ac37a8baadc2d5
Julian da Costa Faro <juliandacostafaro@gmail.com>
2023-03-27 11:59:00 -0300

    add controller test: transfer method

--- springboot_test/src/test/java/org/testing/springboot/app/controllers/AccountControllerTest.java
+++ springboot_test/src/test/java/org/testing/springboot/app/controllers/AccountControllerTest.java
@@ -1,16 +1,24 @@
 package org.testing.springboot.app.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testing.springboot.app.dtos.TransferDTO;
 import org.testing.springboot.app.services.AccountService;
 import org.testing.springboot.app.utils.Data;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(AccountController.class)
@@ -22,6 +30,13 @@ class AccountControllerTest {
     @MockBean
     AccountService accountService;
 
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        objectMapper = new ObjectMapper();
+    }
+
     @Test
     void testDetail() throws Exception {
         when(accountService.findById(1l)).thenReturn(Data.createAccount001());
@@ -31,5 +46,21 @@ class AccountControllerTest {
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.person").value("Andres"))
                 .andExpect(jsonPath("$.balance").value("1000"));
+    }
+
+    @Test
+    void testTransfer() throws Exception {
+        TransferDTO dto = new TransferDTO();
+        dto.setSourceAccountId(1l);
+        dto.setDestinationAccountId(2l);
+        dto.setAmount(new BigDecimal("100"));
+        dto.setBankId(1l);
+
+        mockMvc.perform(post("/api/accounts/transfer").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dto)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.date").value(LocalDate.now().toString()))
+                .andExpect(jsonPath("$.transfer.sourceAccountId").value(1l));
     }
 }
